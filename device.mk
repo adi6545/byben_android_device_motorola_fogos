71c997abe570c55b28e532af4cd9198a819b298b
Georg Veichtlbauer <georg@vware.at>
2024-01-30 09:29:36 +0100

    fogos: powerhint: Add DT2W
    Change-Id: I7d0dfe249798864a39afd4f339b692584faaf3ec

--- device.mk
+++ device.mk
@@ -323,8 +323,6 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
     libqti-perfd-client
 
-$(call soong_config_set,qtipower,tap_to_wake_node,/sys/class/touchscreen/primary/gesture)
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
